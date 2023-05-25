8e91fe88659ddaf31ca9803ce0dcd1fea994b5b7
DinoKnezevic1 <knezevic7p@gmail.com>
2023-05-25 12:24:46 +0200

    Create Controller and Get()

---
+++ WebApi/WebApi/Controllers/FightsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class FightsController : ApiController
+    {
+        
+        // GET api/fights
+        //[System.Web.Http.Route("api/fights/all")]
+        //public IEnumerable<Fight> Get()
+        //{
+        //    return FightsRepository.GetFights();
+        //}
+
+        // GET api/fights/
+        //[System.Web.Http.Route("api/fights/{id}")]
+        public IHttpActionResult Get(int id)
+        {
+            Fight fight = FightsRepository.GetFight(id);
+            if (fight != null)
+            {
+                return Ok(fight);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        //[System.Web.Http.Route("api/fights/getall")]
+        public IHttpActionResult Get(bool includeAddress = false)
+        {
+            IEnumerable<Fight> fights = null;
+
+            fights = FightsRepository.GetFights();
+
+            if (fights.Count() != 0)
+            {
+                return Ok(fights);
+            }
+            return NotFound();
+        }
+
+    }
+}
---
+++ WebApi/WebApi/Models/FightsRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public static class FightsRepository
+    {
+        private static List<Fight> fights = new List<Fight> {
+            new Fight(1, "Dino", "Joshua","Dino", 135000),
+            new Fight(2, "Mike Tyson", "Dino","Dino", 250000),
+            new Fight(3, "Jelena", "Connor McGregor", "Jelena",500000) 
+        };
+
+        public static IEnumerable<Fight> GetFights()
+        {
+            return fights;
+        }
+        public static Fight GetFight(int id)
+        {
+            return fights.Find(fight=>fight.Id==id);
+        }
+    }
+}
